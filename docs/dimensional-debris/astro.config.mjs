f078e314fecf386d6d83cff6c60ff1601d8d63bb
eversiege <poddubtsev.iliya98@gmail.com>
2024-10-17 20:26:44 +0000

    Switch to astro/Starlight
    Co-authored-by: David Hrabě <sproott@users.noreply.github.com>

---
+++ docs/dimensional-debris/astro.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'ULTMOS',
+			logo: {
+				src: './src/assets/ULTMOS.png',
+			},
+			social: {
+				github: 'https://github.com/Coopydood/ultimate-macOS-KVM',
+				discord: 'https://discord.gg/WzWkSsT',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
