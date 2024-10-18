d0e192b1cd0d56522fcada3e3bbcf93f08212988
sproott <david.hrabe12@seznam.cz>
2024-10-18 11:29:52 +0200

    changelog ordering in sidebar

--- docs/dimensional-debris/astro.config.mjs
+++ docs/dimensional-debris/astro.config.mjs
@@ -1,40 +1,41 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'ULTMOS',
-			logo: {
-				src: './src/assets/ULTMOS.png',
-			},
-			customCss: [
-				'./src/styles/custom.css',
-			  ],
-			social: {
-				github: 'https://github.com/Coopydood/ultimate-macOS-KVM',
-				discord: 'https://discord.gg/WzWkSsT',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Changelogs',
-					autogenerate: { directory: 'changelogs' },
-					collapsed: true,
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "ULTMOS",
+      logo: {
+        src: "./src/assets/ULTMOS.png",
+      },
+      customCss: ["./src/styles/custom.css"],
+      social: {
+        github: "https://github.com/Coopydood/ultimate-macOS-KVM",
+        discord: "https://discord.gg/WzWkSsT",
+      },
+      components: {
+        Sidebar: "./src/components/Sidebar.astro",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Changelogs",
+          autogenerate: { directory: "changelogs" },
+          collapsed: true,
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
