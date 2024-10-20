c060018eee8b128376d3dc2884970c004cd5e8c5
Coopydood <39441479+Coopydood@users.noreply.github.com>
2024-10-20 16:30:56 +0100

    Sync to main repo (#133)
    * changelog ordering in sidebar
    
    * basic page layout
    
    Co-authored-by: eversiege <eversiege@users.noreply.github.com>
    
    * fix layout
    
    * implement basic macOS layout
    
    * increase sidebar opacity
    
    * decrease window padding
    
    * Fix mobile view, blur sidebar
    
    * Refactor main content background styling
    
    * Begin moving header
    
    ---------
    
    Co-authored-by: sproott <david.hrabe12@seznam.cz>
    Co-authored-by: eversiege <eversiege@users.noreply.github.com>
    Co-authored-by: eversiege <poddubtsev.iliya98@gmail.com>

--- docs/dimensional-debris/astro.config.mjs
+++ docs/dimensional-debris/astro.config.mjs
@@ -1,40 +1,47 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'ULTMOS',
-			logo: {
-				src: './src/assets/ULTMOS.png',
-			},
-			customCss: [
-				'./src/styles/custom.css',
-			  ],
-			social: {
-				github: 'https://github.com/Coopydood/ultimate-macOS-KVM',
-				discord: 'https://discord.gg/WzWkSsT',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Changelogs',
-					autogenerate: { directory: 'changelogs' },
-					collapsed: true,
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "ULTMOS",
+      logo: {
+        src: "./src/assets/ULTMOS.png",
+      },
+      customCss: ["./src/styles/custom.css", "./src/tailwind.css"],
+      social: {
+        github: "https://github.com/Coopydood/ultimate-macOS-KVM",
+        discord: "https://discord.gg/WzWkSsT",
+      },
+      components: {
+        Sidebar: "./src/components/Sidebar.astro",
+        PageFrame: "./src/components/PageFrame.astro",
+        TwoColumnContent: "./src/components/TwoColumnContent.astro",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+        {
+          label: "Changelogs",
+          autogenerate: { directory: "changelogs" },
+          collapsed: true,
+        },
+      ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
