725132e13de058e19e1a7c8d8ee2a5afd52559f0
Hydra Kutaragi <183824532+KitsuneBalloon@users.noreply.github.com>
2025-01-23 14:46:22 +0000

    Update fedora.sh...again...and again...and again...
    I hope this just fucking works now.
    
    Signed-off-by: Hydra Kutaragi <183824532+KitsuneBalloon@users.noreply.github.com>

--- scripts/extras/fedora.sh
+++ scripts/extras/fedora.sh
@@ -8,5 +8,5 @@ sudo semanage fcontext -a -t virt_image_t 'OVMF_CODE.fd'
 sudo restorecon -v 'OVMF_CODE.fd'
 sudo ausearch -c 'rpc-virtqemud' --raw | audit2allow -M my-rpcvirtqemud
 sudo semodule -i my-rpcvirtqemud.pp
-sudo ausearch -c 'qemu-system-x86' --raw | audit2allow -M my-qemusystemx86.pp
-sudo semodule -X 300 my-qemusystemx86.pp
+sudo ausearch -c 'qemu-system-x86' --raw | audit2allow -M my-qemusystemx86
+sudo semodule -X 300 -i my-qemusystemx86.pp
