e97e125e1a42adba5ba3ff404c5d910c4ffe6acd
Hydra Kutaragi <183824532+KitsuneBalloon@users.noreply.github.com>
2025-01-23 14:38:46 +0000

    Updated once again, 3rd times a charm?
    I surely hope...for my own sanity.
    
    Signed-off-by: Hydra Kutaragi <183824532+KitsuneBalloon@users.noreply.github.com>

--- scripts/extras/fedora.sh
+++ scripts/extras/fedora.sh
@@ -4,8 +4,8 @@
 # Needs to be run with sudo.
 # * = Testing, script is not ready until conformation.
 
-semanage fcontext -a -t virt-image_t 'OVMF_CODE.fd'
-restorecon -v 'OVMF_CODE.fd'
+sudo semanage fcontext -a -t virt_image_t 'OVMF_CODE.fd'
+sudo restorecon -v 'OVMF_CODE.fd'
 sudo ausearch -c 'rpc-virtqemud' --raw | audit2allow -M my-rpcvirtqemud
 sudo semodule -i my-rpcvirtqemud.pp
 sudo ausearch -c 'qemu-system-x86' --raw | audit2allow -M my-qemusystemx86.pp
