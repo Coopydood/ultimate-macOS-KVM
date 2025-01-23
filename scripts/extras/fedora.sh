c014257ad4f5a1302ec4c11d8da87387a97c82e9
Hydra Kutaragi <183824532+KitsuneBalloon@users.noreply.github.com>
2025-01-23 14:57:59 +0000

    Fedora attempted fixes - This genuinely isn't working now...
    I swear i'm missing something...
    
    Signed-off-by: Hydra Kutaragi <183824532+KitsuneBalloon@users.noreply.github.com>

--- scripts/extras/fedora.sh
+++ scripts/extras/fedora.sh
@@ -8,5 +8,10 @@ sudo semanage fcontext -a -t virt_image_t 'OVMF_CODE.fd'
 sudo restorecon -v 'OVMF_CODE.fd'
 sudo ausearch -c 'rpc-virtqemud' --raw | audit2allow -M my-rpcvirtqemud
 sudo semodule -i my-rpcvirtqemud.pp
+sudo semanage fcontext -a -t virt_image_t 'OVMF_CODE.fd'
+sudo restorecon -v 'OVMF_CODE.fd'
 sudo ausearch -c 'qemu-system-x86' --raw | audit2allow -M my-qemusystemx86
 sudo semodule -X 300 -i my-qemusystemx86.pp
+# This next line may need to be tweaked to be a universal file path to the OVMF file included in the repository
+sudo semanage fcontext -a -t virt_image_t '/home/auorafirewood/ultimate-macOS-KVM/ovmf/OVMF_CODE.fd'
+sudo ausearch -c 'qemu-system-x86' --raw | audit2allow -M my-qemusystemx86
